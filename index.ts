afda2f7f493570fb5efd00f4f3bb73be1fd91d24
Antonette Caldwell <pullmana8@gmail.com>
2023-10-05 15:36:28 -0500

    chore: Copied over TODO

--- index.ts
+++ index.ts
@@ -8,6 +8,23 @@ import bodyParser from "body-parser";
 
     app.use(bodyParser.json());
 
+    // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+    // GET /filteredimage?image_url={{URL}}
+    // endpoint to filter an image from a public url.
+    // IT SHOULD
+    // 1. validate the image_url query
+    // 2. call filterImageFromURL(image_url) to filter the image
+    // 3. send the resulting file in the response
+    // 4. deletes any files on the server on finish of the response
+    // QUERY PARAMETERS
+    // image_url: URL of a publicly accessible image
+    // RETURNS
+    // the filtered image [!!TIP res.sendFile(filteredpath); might be useful]
+
+    /*****************************************************************************/
+
+    //! END @TODO1
+
     app.get("/", async (req, res) => {
         res.send("Try GET /filteredimage?image_url={{}}")
     });
